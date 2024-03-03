c8dc76512a927ac6afeceb6afd82c83e9d044bce
Jesus Wladimir Avalos Quizhpi <128276289+jwavalosq@users.noreply.github.com>
2024-03-03 06:32:17 +0000

    Initial commit to plots

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("😃👨‍💻Hello World, This is a test for an Analytics App")
+
+st.write("You will have an application full of projects that will help you implement analytics concepts into real-world business/industry cases")
